f9d2b9b68e42cf2cfb25b14812a77ce5964ae6e1
chrns <lnrdprime@gmail.com>
2016-09-29 18:42:13 +0500

    fix main.c in lesson 05 again...

--- code/05/main.c
+++ code/05/main.c
@@ -12,8 +12,8 @@ int main(void) {
 void init_mcu() {
 	init_pll();
 	init_leds();
-//	SysTick->LOAD = SystemCoreClock /1000 - 1;
-//	SysTick->VAL = SystemCoreClock /1000 - 1;
+//	SysTick->LOAD = 24000000UL/1000 - 1;
+//	SysTick->VAL = 24000000UL/1000 - 1;
 //	SysTick->CTRL= SysTick_CTRL_CLKSOURCE_Msk |
 //	                SysTick_CTRL_TICKINT_Msk   |
 //	                SysTick_CTRL_ENABLE_Msk;
