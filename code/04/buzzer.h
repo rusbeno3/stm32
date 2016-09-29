65944c012d8d933b12baaa52607742b420f0800e
chrns <lnrdprime@gmail.com>
2016-09-29 17:39:57 +0500

    add lesson 04: GPIO + LED

---
+++ code/04/buzzer.h
@@ -0,0 +1,6 @@
+#ifndef __BUZZER_H__
+#define __BUZZER_H__
+
+#include "stm32f10x.h"
+
+#endif /* __BUZZER_H__ */
---
+++ code/04/utils.h
@@ -0,0 +1,8 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include "stm32f10x.h"
+
+void init_pll(void);
+
+#endif /* __UTILS_H__ */
